cb63e86ce1e4273c0cf8135c8e58216f01d66d61
maziokey <okeyluvsu2004@yahoo.com>
2020-08-07 18:10:37 +0100

    image field added

--- books/models.py
+++ books/models.py
@@ -9,6 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
@@ -17,9 +18,9 @@ class Book(models.Model):
         return reverse('book_detail', args=[str(self.id)])
 
 class Review(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews',)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
     review = models.CharField(max_length=255)
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.review
